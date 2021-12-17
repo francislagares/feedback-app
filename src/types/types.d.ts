09011bf5c8266df241bf94043054e4395742fb31
Francis Lagares <francis.lagares@gmail.com>
2021-12-17 18:11:46 +0100

    feat: define Submit and NewFeedback types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -4,5 +4,12 @@ interface IFeedback {
   text: string;
 }
 
+type NewFeedback = {
+  id?: number;
+  text: string;
+  rating: number;
+};
+
 type ChangeInput = React.ChangeEvent<HTMLInputElement>;
+type Submit = React.FormEvent<HTMLFormElement>;
 type ButtonType = 'button' | 'submit' | 'reset' | undefined;
