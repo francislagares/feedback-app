d669e360273f99fe108a592e18ab72823ae92478
Francis Lagares <francis.lagares@gmail.com>
2021-12-18 18:52:47 +0100

    feat: opacity animation on FeedbackItem

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import FeedbackItem from './FeedbackItem';
 
 interface IProps {
@@ -12,9 +13,22 @@ const FeedbackList = ({ feedback, handleDelete }: IProps): JSX.Element => {
   }
   return (
     <div className='feedback-list'>
-      {feedback.map(item => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map(item => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 };
